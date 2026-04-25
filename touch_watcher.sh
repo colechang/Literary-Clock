1dbfc3ed0c0ac58ccfe70b740a7122a48e5aa510
colechang <colechang10@gmail.com>
2026-04-24 23:43:04 -0400

    Scratch that evtest wasn't as consistent as I'd like. Moving back to reading raw touch events

--- touch_watcher.sh
+++ touch_watcher.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
-killall evtest 2>/dev/null
-evtest /dev/input/event1 | while read LINE; do
-    case "$LINE" in
-        *"p: 0"*)
-            # ignore lift events
-            ;;
-        *"Report Sync"*"p: "*)
-            touch /tmp/litclock_refresh
-            sleep 2
-            ;;
-    esac
+# Read raw touch events directly from input device
+# Each input_event struct is 16 bytes on ARM 32-bit
+# We just need to detect ANY touch activity
+
+while true; do
+    # Read one event (16 bytes)
+    dd if=/dev/input/event1 bs=16 count=1 2>/dev/null
+    # Any read means activity - create refresh flag
+    touch /tmp/litclock_refresh
+    # Debounce - drain remaining events and wait
+    dd if=/dev/input/event1 bs=16 count=10 2>/dev/null
+    sleep 2
 done
