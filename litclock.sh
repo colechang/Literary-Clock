9722a75cc9630403daddf8b8aa86a6e1e233e30e
colechang <colechang10@gmail.com>
2026-04-26 23:58:51 -0400

    Created weather functionality that is displayed on top of the screen
    - wttr api request
    - Only done if wifi is available

--- litclock.sh
+++ litclock.sh
@@ -6,6 +6,9 @@ REGULAR="$FONT_DIR/NotoSerif-Regular.ttf"
 BOLD="$FONT_DIR/NotoSerif-Bold.ttf"
 ITALIC="$FONT_DIR/NotoSerif-Italic.ttf"
 BOLDITALIC="$FONT_DIR/NotoSerif-BoldItalic.ttf"
+WEATHER_CACHE="/tmp/weather_cache.txt"
+CITY="Toronto"
+
 COUNTER=0
 
 while true; do
@@ -14,6 +17,15 @@ while true; do
     killall fmon 2>/dev/null
 
     TIME=$(date +%H:%M)
+    # Fetch weather every 60 minutes
+    if [ $(expr $COUNTER % 60) -eq 0 ]; then
+        FRESH=$(wget -q -T 5 -O - "wttr.in/$CITY?format=3" 2>/dev/null)
+        if [ -n "$FRESH" ]; then
+            echo "$FRESH" > "$WEATHER_CACHE"
+        fi
+    fi
+
+    WEATHER=$(cat "$WEATHER_CACHE" 2>/dev/null || echo "")
 
     # Pick exactly ONE random matching line
     LINE=$(grep "^$TIME|" "$CSV" | awk -v seed="$(date +%s%N)" 'BEGIN{srand(seed)} {lines[NR]=$0} END{if(NR>0) print lines[int(rand()*NR)+1]}')
@@ -54,8 +66,12 @@ while true; do
     HOUR=$(date +%H)
     if [ "$HOUR" -ge 22 ] || [ "$HOUR" -lt 6 ]; then
         $FBINK -q -c -m -M -H -t regular="$REGULAR",bold="$BOLD",italic="$ITALIC",bolditalic="$BOLDITALIC",size=$FONT_SIZE,top=60,bottom=60,left=50,right=50,padding=BOTH,format "$DISPLAY_TEXT"
+
+        [ -n "$WEATHER" ] && $FBINK -q -m -y 1 -S 2 -H "$WEATHER"
     else
         $FBINK -q -c -m -M -t regular="$REGULAR",bold="$BOLD",italic="$ITALIC",bolditalic="$BOLDITALIC",size=$FONT_SIZE,top=60,bottom=60,left=50,right=50,padding=BOTH,format "$DISPLAY_TEXT"
+
+        [ -n "$WEATHER" ] && $FBINK -q -m -y 1 -S 2 "$WEATHER"
     fi
 
     # Check every second for touch refresh signal
